916dc8611a2b474bbabdad0529946f039e92b48d
Sujoy Giri <girisujoy630@gmail.com>
2024-06-25 22:52:35 +0530

    added show hide icon

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,6 @@ import { Routes } from '@angular/router';
 import { SignInComponent } from './sign-in/sign-in.component';
 
 export const routes: Routes = [
-    {path:"sign-in",component:SignInComponent},
-    {path:"**",redirectTo:"/",pathMatch:"full"}
+    { path: "sign-in", component: SignInComponent },
+    // {path:"**",redirectTo:"/",pathMatch:"full"}
 ];
--- src/app/sign-in/sign-in.component.ts
+++ src/app/sign-in/sign-in.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { ButtonModule } from 'primeng/button';
@@ -12,10 +12,10 @@ import { RouterModule } from '@angular/router';
   selector: 'app-sign-in',
   standalone: true,
   imports: [
-    ButtonModule, 
-    InputTextModule, 
-    CardModule, 
-    IconFieldModule, 
+    ButtonModule,
+    InputTextModule,
+    CardModule,
+    IconFieldModule,
     InputIconModule,
     CommonModule,
     RouterModule,
@@ -25,18 +25,33 @@ import { RouterModule } from '@angular/router';
   styleUrl: './sign-in.component.css'
 })
 export class SignInComponent implements OnInit {
-  signinForm!:FormGroup;
-  constructor(private formBuilder:FormBuilder){}
+  signinForm!: FormGroup;
+  constructor(private formBuilder: FormBuilder, private elementRef: ElementRef) { }
 
   ngOnInit(): void {
     this.signinForm = this.formBuilder.group({
-      sellername:['',[Validators.required,Validators.minLength(3),Validators.maxLength(50)]],
-      email:['',[Validators.required,Validators.email]],
-      password:['',[Validators.required,Validators.pattern(/^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{6,16}$/)]]
-    })
+      sellername: ['', [Validators.required, Validators.minLength(3), Validators.maxLength(50)]],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.pattern(/^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{6,16}$/)]]
+    });
   }
-  handelFormSubmit(){
+  handelFormSubmit() {
     console.log(this.signinForm.value);
-    
+
+  }
+  showPassword() {
+    let passwordElementNode = this.elementRef.nativeElement.querySelector("#password");
+    let showBtnElementNode = this.elementRef.nativeElement.querySelector("#show-btn");
+    if (showBtnElementNode.innerText === 'Show') {
+
+      showBtnElementNode.classList.add('text-blue-400');
+      showBtnElementNode.classList.remove('text-red-400');
+      showBtnElementNode.innerText = 'Hide';
+    } else {
+      showBtnElementNode.classList.add('text-red-400');
+      showBtnElementNode.classList.remove('text-blue-400');
+      showBtnElementNode.innerText = 'Show';
+    }
+    passwordElementNode.type = passwordElementNode.type === 'password' ? 'text' : 'password';
   }
 }
