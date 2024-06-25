f44ffe06c4f924b72bf6156808dd079f112cdb49
Sujoy Giri <girisujoy630@gmail.com>
2024-06-25 08:10:36 +0530

    added navbar component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { PrimeNGConfig } from 'primeng/api';
+
+import { NavbarComponent } from './navbar/navbar.component';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NavbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
-  title = 'bikri-frontend-seller';
+export class AppComponent implements OnInit {
+  constructor(private primengConfig: PrimeNGConfig) { }
+  ngOnInit(): void {
+    this.primengConfig.ripple = true;
+  }
 }
