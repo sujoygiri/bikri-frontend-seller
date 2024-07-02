423e7842357ff6fadbce387a54c982e6994928d5
Sujoy Giri <girisujoy630@gmail.com>
2024-07-02 17:58:46 +0530

    added auth button hide logic upon going to auth page

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -2,16 +2,20 @@ import { Component, OnInit } from '@angular/core';
 import { MenuItem } from "primeng/api";
 import { MenubarModule } from "primeng/menubar";
 import { ButtonModule } from "primeng/button";
+import { NavigationEnd, Router, RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [MenubarModule, ButtonModule],
+  imports: [MenubarModule, ButtonModule, RouterModule, CommonModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
 export class NavbarComponent implements OnInit {
   items: MenuItem[] | undefined;
+  showSignInBtn: boolean = true;
+  constructor(private router: Router) { }
   ngOnInit(): void {
     this.items = [
       {
@@ -21,7 +25,7 @@ export class NavbarComponent implements OnInit {
       {
         label: 'Add Product',
         icon: 'pi pi-plus-circle',
-        routerLink:'/add-product'
+        routerLink: '/add-product'
       },
       {
         label: 'Projects',
@@ -60,5 +64,12 @@ export class NavbarComponent implements OnInit {
         icon: 'pi pi-envelope'
       }
     ];
+    this.router.events.subscribe({
+      next: (value) => {
+        if (value instanceof NavigationEnd) {
+          (value.url === '/sign-in' || value.url === '/sign-up') ? this.showSignInBtn = false : this.showSignInBtn = true;
+        }
+      },
+    });
   }
 }
